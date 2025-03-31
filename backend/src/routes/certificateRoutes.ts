5ab4210dcf432e1fb981c39e9149a1366c3425de
Sharon Kitavi <sharonkmwikali@gmail.com>
2025-03-31 23:24:47 +0300

    Create files to handle API routes and upload metadata to IPFS
    Changes made to mintPOAP.js:
    - Removed auto-execution so it doesn’t run when imported.
    - Added return response.data; instead of logging.
    - Generated a random secret code instead of using a fixed 123456.

---
+++ backend/src/routes/certificateRoutes.ts
@@ -0,0 +1,10 @@
+/*import express from "express";
+import { issuePOAP } from "../controllers/poapController";
+
+const router = express.Router();
+
+// Route to issue a POAP
+router.post("/issue", issuePOAP);
+
+export default router;
+*/
