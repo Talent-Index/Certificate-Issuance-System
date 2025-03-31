2e430a0c4256d6670117190909d83069e7d059b5
I-Macharia <gichoya.macharia@gmail.com>
2025-04-01 00:32:11 +0300

    Update Hardhat configuration for Solidity version and network settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,16 +2,24 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: "0.8.26",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
     fuji: {
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      url: "https://open-campus-codex-sepolia.drpc.org",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 43113,
+      chainId: 656476,
     },
+    // localhost: {
+    //   url: "http://127.0.0.1:8545",
+    // },
+    // fuji: {
+    //   url: "https://api.avax-test.network/ext/bc/C/rpc",
+    //   accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    //   chainId: 43113,
+    // },
   },
   etherscan: {
     apiKey: process.env.SNOWTRACE_API_KEY,
